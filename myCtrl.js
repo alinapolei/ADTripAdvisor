5c3c788ce03c7f9302d8b547ca2343c797200f71
alina <alinapolei96@gmail.com>
2019-06-13 13:23:48 +0300

    started

---
+++ myCtrl.js
@@ -0,0 +1,8 @@
+var app = angular.module('myApp', []);
+serverUrl = "http://localhost:3000"
+app.controller('myCtrl', function($scope, $http) {
+    $http.get(serverUrl + "/getCountries")
+        .then(function(response) {
+            $scope.answer = response.data;
+        });
+});
