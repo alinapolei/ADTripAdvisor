9498b8f1b9cffd561dd6de9af4b1a67ff2ffe5db
Dror <droruri3@gmail.com>
2019-06-14 13:23:40 +0300

    commit before pull

--- pages/user/register/regController.js
+++ pages/user/register/regController.js
@@ -1,8 +1,13 @@
-angular.module("myApp")
-    .controller("regController", function ($scope) {
+angular.module("myApp" )
+    .controller('regController', function($scope, $http) {
+        serverUrl = "http://localhost:3000"
+        $http.get(serverUrl + "/getCountries")
+        .then(function(response) {
+            $scope.countries = response.data;
+        });
+        $http.get(serverUrl + "/getQuestions")
+            .then(function(response) {
+                $scope.quest = response.data;
+            });
 
-        
-
-
-
-    });
+});
