286d9a1396f5bda706dc61ce23db8d6885fe5e2f
alina <alinapolei96@gmail.com>
2019-06-15 00:57:06 +0300

    finish showing the points in main screen for guest

--- pages/mainScreen/gusetUser/userController.js
+++ pages/mainScreen/gusetUser/userController.js
@@ -2,14 +2,22 @@ serverUrl = "http://localhost:3000";
 angular.module("myApp")
     .controller("userController", function ($scope, $http) {
         $scope.points=[];
-        var i;
-        for (i=0; i<3; i++)
-        {
-            $http.get(serverUrl + "/getRandom")
-                .then(function (response) {
-                    $scope.points.push(response.data);
-                }, function (error) {
-                    alert(error.data);
-                });
-        }
+        $http.get(serverUrl + "/getRandom")
+            .then(function (response) {
+                //$scope.points=response.data;
+                var i;
+                for(i=0; i<3; i++) {
+                    $http({
+                        method: 'GET',
+                        url: serverUrl + '/getPOIInfo?tagId=' + response.data[i].poi_id
+                    }).then(function successCallback(response) {
+                        $scope.points.push(response.data);
+                    }, function errorCallback(error) {
+                        alert(error.data())
+                    });
+                }
+
+            }, function (error) {
+                alert(error.data);
+            });
     });
