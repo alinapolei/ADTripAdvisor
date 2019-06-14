619ec708da6d0168818663511d79e00c9236341c
alina <alinapolei96@gmail.com>
2019-06-14 20:34:45 +0300

    menu bar done

--- pages/user/userLogin/loginController.js
+++ pages/user/userLogin/loginController.js
@@ -1,10 +1,10 @@
 angular.module("myApp")
     .controller("loginController", function ($scope, $http, $window) {
-        $scope.usename;
-        $scope.password;
-
+        //$scope.usename;
+        //$scope.password;
         $scope.submit = function(isValid){
             if(isValid){
+                alert($scope.username + " " + $scope.password);
                 $http.post('/login', {
                     username : $scope.username,
                     password : $scope.password})
