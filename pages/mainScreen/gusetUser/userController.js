44e8c39ee41b2a712ad5182fa88e9261503038ac
alina <alinapolei96@gmail.com>
2019-06-14 22:52:56 +0300

    started main screen

--- pages/mainScreen/gusetUser/userController.js
+++ pages/mainScreen/gusetUser/userController.js
@@ -2,13 +2,12 @@ serverUrl = "http://localhost:3000";
 angular.module("myApp")
     .controller("userController", function ($scope, $http) {
         $scope.points=[];
-        $scope.points.add("a");
         var i;
         for (i=0; i<3; i++)
         {
             $http.get(serverUrl + "/getRandom")
                 .then(function (response) {
-                    $scope.points.add(response.data);
+                    $scope.points.push(response.data);
                 }, function (error) {
                     alert(error.data);
                 });
